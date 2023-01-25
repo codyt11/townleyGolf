6042610029c4225e24ef9061b3b63dd011c8e102
unknown <codyt11@hotmail.com>
2023-01-25 12:59:18 -0600

    adjusted color and alignment of navbar

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './header.css';
 
 function Header() {
-    return(
-        <h1>Hello World!</h1>
-    );
+  return (
+    <Navbar className='nav' expand="lg">
+      <Container className='links navbar-dark'>
+        <Navbar.Brand href="#home">Townley Golf</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="link-title me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Blog</Nav.Link>
+            <Nav.Link href="#link">Course Search</Nav.Link>
+            <Nav.Link href="#link">Hot100</Nav.Link>
+            <Nav.Link href="#link">Leaderboard</Nav.Link>
+            <Nav.Link href="#link">Weather</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
