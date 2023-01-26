78e0f965eb3628bbc8374c477bc947423603c57f
unknown <codyt11@hotmail.com>
2023-01-26 14:00:29 -0600

    started blog page, set up test data and basic designs

---
+++ src/blogs.js
@@ -0,0 +1,21 @@
+
+let blogs = [
+    {
+        title:"Title",
+       content:"this is my test content"
+    }, 
+    {
+        title:"Title2",
+       content:"this is my test content"
+    }, 
+    {
+        title:"Title3",
+       content:"this is my test content"
+    }, 
+    {
+        title:"Title4",
+       content:"this is my test content"
+    } 
+]
+
+export default blogs
--- src/pages/blog/blog.component.jsx
+++ src/pages/blog/blog.component.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Blogs from '../../blogs';
+
+import './blog.css';
+
 
 const Blog = () => (
     <div className="blog">
-        <h1>Blog!</h1>
+        <Container className="blog-contain">
+            <h1 className="title">My Golf Journey!</h1>
+            <div className="blog-post">
+                {Blogs.map((posts) => (
+                    <div className="post" key={posts.title}>
+                        <h3>{posts.title}</h3>
+                        <span>{posts.content}</span>
+                    </div>
+                ))}
+            </div>
+        </Container>
     </div>
 );
 
